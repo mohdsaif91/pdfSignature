5028bfa9007a06b64f25f4af1a07e8804ca5f5fc
mohdsaif91 <saiffarooqui9505@gmail.com>
2023-02-07 18:27:31 +0530

    signature done on pdf

--- src/App.js
+++ src/App.js
@@ -1,43 +1,127 @@
-import React, { useRef, useEffect } from 'react';
-import WebViewer from '@pdftron/webviewer';
-import './App.css';
+import React, { useRef, useEffect, useState } from "react";
+import WebViewer from "@pdftron/webviewer";
+import { BlendMode, degrees, PDFDocument, rgb, StandardFonts } from "pdf-lib";
+import download from "downloadjs";
+import SignatureCanvas from "react-signature-canvas";
+
+import "./App.css";
 
 const App = () => {
-  const viewer = useRef(null);
+  const [pdfInfo, setPdfInfo] = useState();
+  const [signature, setSignature] = useState();
+  const [position, setPosition] = useState({ x: 0, y: 0 });
 
-  // if using a class, equivalent of componentDidMount 
-  useEffect(() => {
-    WebViewer(
-      {
-        path: '/webviewer/lib',
-        initialDoc: '/files/PDFTRON_about.pdf',
-      },
-      viewer.current,
-    ).then((instance) => {
-      const { documentViewer, annotationManager, Annotations } = instance.Core;
+  const signatureRef = useRef(null);
 
-      documentViewer.addEventListener('documentLoaded', () => {
-        const rectangleAnnot = new Annotations.RectangleAnnotation({
-          PageNumber: 1,
-          // values are in page coordinates with (0, 0) in the top left
-          X: 100,
-          Y: 150,
-          Width: 200,
-          Height: 50,
-          Author: annotationManager.getCurrentUser()
-        });
+  const modifyPDF = async () => {
+    const url = "/files/second.pdf";
+    console.log(url);
+    const existingPdfBytes = await fetch(url).then((res) => res.arrayBuffer());
+    const pdfDoc = await PDFDocument.load(existingPdfBytes);
 
-        annotationManager.addAnnotation(rectangleAnnot);
-        // need to draw the annotation otherwise it won't show up until the page is refreshed
-        annotationManager.redrawAnnotation(rectangleAnnot);
-      });
+    const pages = pdfDoc.getPages();
+    const firstPage = pages[0];
+    const { width, height } = firstPage.getSize();
+
+    const pngImage = await pdfDoc.embedPng(signature);
+    const pngDims = pngImage.scale(0.5);
+    firstPage.drawImage(pngImage, {
+      x: parseInt(position.x),
+      y: parseInt(position.y),
+      width: pngDims.width,
+      height: pngDims.height,
     });
-  }, []);
+
+    firstPage.drawText(`sign above`, {
+      x: parseInt(position.x),
+      y: parseInt(position.y),
+      size: 15,
+    });
+
+    // Serialize the PDFDocument to bytes (a Uint8Array)
+    const pdfData = await pdfDoc.save();
+    const bytes = new Uint8Array(pdfData);
+    const blob = new Blob([bytes], { type: "application/pdf" });
+    const docUrl = URL.createObjectURL(blob);
+    // const docUrl = URL.createObjectURL(
+    //   new Blob(pdfData, { type: "application/pdf" })
+    // );
+    // console.log(docUrl);
+    setPdfInfo(docUrl);
+
+    // Trigger the browser to download the PDF document
+    // download(pdfData, "pdf-lib_modification_example.pdf", "application/pdf");
+  };
+
+  const saveSignature = () => {
+    const base64Img = signatureRef.current.toDataURL();
+    setSignature(base64Img);
+  };
+
+  const getTrimSignature = () => {
+    const trimBase64Img = signatureRef.current.getTrimmedCanvas();
+    console.log(trimBase64Img.toDataURL());
+  };
+
+  // console.log(signature);
 
   return (
     <div className="App">
       <div className="header">React sample</div>
-      <div className="webviewer" ref={viewer}></div>
+      <div className="canvas-container">
+        <div>
+          <h2>Get signature Position</h2>
+          <div>
+            <label>X(Horizontal) Position</label>
+            <input
+              onChange={(e) => setPosition({ ...position, x: e.target.value })}
+              value={position.x}
+              type="number"
+            />
+          </div>
+          <div>
+            <label>Y(Verticle) Position</label>
+            <input
+              value={position.y}
+              onChange={(e) => setPosition({ ...position, y: e.target.value })}
+              type="number"
+            />
+          </div>
+          <div>
+            <button onClick={() => modifyPDF()}>DOWNLOAD THE PDF</button>
+          </div>
+        </div>
+        <div className="signature-container">
+          <div className="signature-pad">
+            <SignatureCanvas
+              // penColor="green"
+              ref={signatureRef}
+              canvasProps={{ height: 200, className: "sigCanvas" }}
+              onEnd={saveSignature}
+            />
+          </div>
+          <div className="btn-container">
+            <button onClick={() => getTrimSignature()}>
+              Trim the white space
+            </button>
+            <button>Clear Signature</button>
+          </div>
+        </div>
+      </div>
+      <>
+        {
+          <iframe
+            height="700"
+            title="test-frame"
+            src={pdfInfo}
+            type="application/pdf"
+          />
+        }
+      </>
+
+      {/* <button onClick={saveSignature}>Save Signature on PDf</button> */}
+
+      {/* <div className="webviewer" ref={viewer}></div> */}
     </div>
   );
 };
